5c38c22b867ad21c84c800c56da4dbc6a09efa8d
jaisonmf <85539202+jaisonmf@users.noreply.github.com>
2022-04-29 12:03:42 +1000

    trees, temple, maze script, patrol
    i have just over 3000 trees

---
+++ Assets/Scripts/DangerZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZone : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+
+
+    void OnTriggerEnter(Collider checkpoint)
+    {
+        if (checkpoint.tag == "Checkpoint")
+        {
+            respawnPoint = checkpoint.transform.position;
+        }
+
+        if (checkpoint.gameObject.tag == "Reset")
+        {
+            transform.position = respawnPoint;
+            Debug.Log("you stupid");
+        }
+    }
+
+}
